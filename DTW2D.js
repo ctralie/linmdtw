fbd6494413c4b1b21a31e950ccbd2170bcf06bdd
Chris Tralie <chris.tralie@gmail.com>
2019-10-17 15:46:44 -0400

    Debugging with Liz

--- DTW2D.js
+++ DTW2D.js
@@ -41,7 +41,7 @@ function matchPoints(xpoints, ypoints, isSubSequence){
             
                 //the total distance will come from the minimum of the three options added to the distance
                 accumulated_dists[i][j] = (Math.min(accumulated_dists[i-1][j-1], accumulated_dists[i-1][j], 
-                accumulated_dists[i][j-1], accumulated_dists[i][j]) + distances[i][j]);
+                accumulated_dists[i][j-1]) + distances[i][j]);
             
             }
         }
@@ -78,27 +78,25 @@ function matchPoints(xpoints, ypoints, isSubSequence){
        }while(i>0 || j>0)
 
     }else if (isSubSequence){
-        var subDists = [];
-        subDists[0][0] = (distances[0][0]);
+        var subDists = [[distances[0][0]]];
 
         //fill first row
-        for (var i = 1; i < ypoints.length - 1; i++){
+        for (let i = 1; i <= ypoints.length - 1; i++){
             subDists[0][i] = distances[0][i];
         }
 
         //fill first col
-        for(var i = 0; i < xpoints.length - 1; i++){
-            subDists[i][0] = distances[i][0];
+        for(let i = 1; i <= xpoints.length -1; i++){
+            subDists.push([]);
+            subDists[i][0] = distances[i][0] + subDists[i-1][0];
         }
 
-        //make the sub sequence specific "accumulated distancs" array
-        for(var i = 1; i <= xpoints.length -1; i++){
-            for(var j = 1; j <= ypoints.length -1; j++){
-                
+        //make the sub sequence specific "accumulated distances" array
+        for(let i = 1; i <= xpoints.length -1; i++){
+            for(let j = 1; j <= ypoints.length -1; j++){
                 //the total distance will come from the minimum of the three options added to the distance
-                subDists[i][j] = (Math.min(subDists[i-1][j-1], subDists[i-1][j], 
-                    subDists[i][j-1], subDists[i][j]) + distances[i][j]);
-    
+                subDists[i][j] = Math.min(subDists[i-1][j-1], subDists[i-1][j], 
+                                            subDists[i][j-1]) + distances[i][j];
             }
         }
         matches = subSequence(subDists, matches);
@@ -118,28 +116,27 @@ function computeDistance(x1, y1, x2, y2){
 
 function subSequence(subDists, matches){
     //finding the minimum element at the bottom row
-    var i = 0;
     //variable to hold the minimum value
-    var minimum = 0;
+    let minimumVal = subDists[xpoints.length-1][0];
+    let minimum = [xpoints.length-1, 0];
 
     //maybe put xpoint.length - 1 into a variable so you don't keep referencing it and looking it up
 
     //finding the minimum value on the bottom row
-    do{
 
-        for(var i = 0; i < xpoints.length - 1; i++){
-            //if the current distance is less than the next, then it is the minimum
-            if(subDists[xpoints.length-1][i] < subDists[xpoints.length-1][i+1]){
-                //minimum is found and is stored in a variable
-                minimum = [xpoints.length - 1, i];
-            }
+    for(let j = 1; j < ypoints.length; j++){
+        //if the current distance is less than the minimum so far
+        if(subDists[xpoints.length-1][j] < minimumVal){
+            //minimum is found and is stored in a variable
+            minimumVal = subDists[xpoints.length-1][j];
+            minimum = [xpoints.length - 1, j];
         }
-
-        i++;
-    }while (i < subDists[0].length && minimum == 0);
+    }
 
     //backtracing to the top row
     matches.push(minimum);  //should be the last element
+    let i = minimum[0];
+    let j = minimum[1];
 
     do{
         //if either i or j are zero, we know we can only go back in one direction from there
