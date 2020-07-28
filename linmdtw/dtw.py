f2859d24f15addccd47d9ed1647965b998ff353d
Chris Tralie <chris.tralie@gmail.com>
2020-07-28 11:37:54 -0400

    Adding unit tests, fixing CPU fallback

--- linmdtw/dtw.py
+++ linmdtw/dtw.py
@@ -23,6 +23,10 @@ def dtw_brute(X, Y, debug=False):
     if not Y.dtype == np.float32:
         warnings.warn("Y is not 32-bit, so creating 32-bit version")
         Y = np.array(Y, dtype=np.float32)
+    if X.shape[0] < X.shape[1]:
+        warnings.warn("X has more columns than rows; did you mean to transpose?")
+    if Y.shape[0] < Y.shape[1]:
+        warnings.warn("Y has more columns than rows; did you mean to transpose?")
     return DTW(X, Y, int(debug))
 
 def dtw_brute_backtrace(X, Y, debug=False):
@@ -190,11 +194,16 @@ def linmdtw(X, Y, box = None, min_dim = 500, do_gpu = True, metadata = None):
     if not Y.dtype == np.float32:
         warnings.warn("Y is not 32-bit, so creating 32-bit version")
         Y = np.array(Y, dtype=np.float32)
+    if X.shape[0] < X.shape[1]:
+        warnings.warn("X has more columns than rows; did you mean to transpose?")
+    if Y.shape[0] < Y.shape[1]:
+        warnings.warn("Y has more columns than rows; did you mean to transpose?")
     dtw_diag_fn = dtw_diag
     if do_gpu:
-        from .dtwgpu import DTW_GPU_Initialized, DTW_GPU_Failed, init_gpu, dtw_diag_gpu
+        from .dtwgpu import DTW_GPU_Initialized, init_gpu, dtw_diag_gpu
         if not DTW_GPU_Initialized:
             init_gpu()
+        from .dtwgpu import DTW_GPU_Failed
         if DTW_GPU_Failed:
             warnings.warn("Falling back to CPU")
             do_gpu = False
---
+++ test/test_dtw.py
@@ -0,0 +1,46 @@
+import pytest
+import numpy as np
+import scipy.interpolate as interp
+import matplotlib.pyplot as plt
+import linmdtw
+
+class TestLibrary:
+    # Does the library install in scope? Are the objects in scope?
+    def test_import(self):
+        import ripser
+        from ripser import ripser, Rips
+        assert 1
+
+
+class TestDTW:
+    def test_input_type_warning(self):
+        np.random.seed(0)
+        X = np.random.rand(10, 3)
+        with pytest.warns(UserWarning, match="is not 32-bit, so creating 32-bit version") as w:
+            linmdtw.linmdtw(X, X)
+        with pytest.warns(UserWarning, match="is not 32-bit, so creating 32-bit version") as w:
+            linmdtw.dtw_brute_backtrace(X, X)
+
+    def test_dimension_warning(self):
+        np.random.seed(0)
+        X = np.random.rand(3, 10)
+        with pytest.warns(UserWarning, match="has more columns than rows") as w:
+            linmdtw.linmdtw(X, X)
+        with pytest.warns(UserWarning, match="has more columns than rows") as w:
+            linmdtw.dtw_brute_backtrace(X, X)
+
+    def test_cpu_vs_gpu(self):
+        N = 1000
+        t1 = np.linspace(0, 1, N)
+        t2 = t1**2
+        X = np.zeros((N, 2), dtype=np.float32)
+        X[:, 0] = np.cos(2*np.pi*t1)
+        X[:, 1] = np.sin(4*np.pi*t1)
+        Y = np.zeros_like(X)
+        Y[:, 0] = np.cos(2*np.pi*t2)
+        Y[:, 1] = np.sin(4*np.pi*t2)
+        path1 = linmdtw.linmdtw(X, Y)
+        path1 = np.array(path1)
+        path2 = linmdtw.dtw_brute_backtrace(X, Y)
+        path2 = np.array(path2)
+        assert(np.allclose(path1, path2))
